f6fdde852468254c486064b339a3eb70c50d4d92
Amanksingh16 <amansingh16597@gmail.com>
2018-01-22 14:32:53 -0800

    Main Run Success

---
+++ Waveform_Classes_Objects.java
@@ -0,0 +1,13 @@
+public class Waveform_Classes_Objects
+{
+public static void main(String[] args) {
+		int a[]= {3,5,9,7,8,4};
+		Waveform w = new Waveform();
+		w.sort(a);
+		for(int i=0; i<a.length;i++)
+		{
+			System.out.print(a[i]);
+		}
+	}
+}
+
