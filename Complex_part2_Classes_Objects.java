39eab7e6a8bd562750da5db9277e817dfd65f697
Amanksingh16 <amansingh16597@gmail.com>
2018-01-22 14:30:17 -0800

    Main Run Success

---
+++ Complex_part2_Classes_Objects.java
@@ -0,0 +1,13 @@
+
+public class Complex_part2_Classes_Objects {
+
+	public static void main(String[] args) {
+		Complex_part2 C = new Complex_part2(4, 5);
+		Complex_part2 C2 = new Complex_part2(1, 2);
+		C.multiply(C2);
+		C.plus(C2);
+		C.print();
+		
+	}
+
+}
